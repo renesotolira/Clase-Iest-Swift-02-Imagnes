3ba04740c07633bf514071485b6d91c26cc27fe4
René <rene.soto01@iest.edu.mx>
2023-09-05 10:26:18 -0600

    Initial Commit

---
+++ Imagenes/ImagenesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagenesApp.swift
+//  Imagenes
+//
+//  Created by Rene SL on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ImagenesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
